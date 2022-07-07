c912876df88ada961dd2ca9fc54a25fac89db87c
chinatsu <459293631@qq.com>
2022-07-07 23:08:54 +0800

    add router

--- src/main.js
+++ src/main.js
@@ -1,8 +1,16 @@
 import Vue from 'vue'
+import {Button, Select} from 'element-ui'
 import App from './App.vue'
+// import 'element-ui/lib/theme-chalk/index.css'
 
+import router from '../router'
+
+Vue.use(Button)
+Vue.use(Select)
 Vue.config.productionTip = false
 
 new Vue({
+  router,
+  el: '#app',
   render: h => h(App),
 }).$mount('#app')

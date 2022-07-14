ce6064b82feb70174816ad2f2a54f9e0d5f240be
Choy <choy@B-24QRQ6LT-0037.local>
2022-07-14 15:23:58 +0800

    组件订单展示

--- views/home/index.vue
+++ views/home/index.vue
@@ -21,6 +21,22 @@
         </el-table>
       </el-card>
     </el-col>
+    <el-col style="margin-top: 20px;" :span="16">
+      <div class="num">
+        <el-card v-for="item in countData" :key="item.name" :body-style="{ display: 'flex', padding: 0 }">
+          <i class="icon" :class="`el-icon-${item.icon}`" :style="{ background: item.color }"></i>
+          <div class="detail">
+            <p class="num">¥{{ item.value }}</p>
+            <p class="txt">{{ item.name }}</p>
+          </div>
+        </el-card>
+      </div>
+      <el-card style="height: 280px"></el-card>
+      <div class="graph">
+        <el-card style="height: 265px"></el-card>
+        <el-card style="height: 265px"></el-card>
+      </div>
+    </el-col>
   </el-row>
 </template>
 
@@ -73,7 +89,45 @@ export default {
         todayBuy: '今日购买',
         monthBuy: '本月购买',
         totalBuy: '总购买'
-      }
+      },
+      countData: [
+        {
+          name: "今日支付订单",
+          value: 1234,
+          icon: "success",
+          color: "#2ec7c9"
+        },
+        {
+          name: "今日收藏订单",
+          value: 210,
+          icon: "star-on",
+          color: "#ffb980"
+        },
+        {
+          name: "今日未支付订单",
+          value: 1234,
+          icon: "s-goods",
+          color: "#5ab1ef"
+        },
+        {
+          name: "本月支付订单",
+          value: 1234,
+          icon: "success",
+          color: "#2ec7c9"
+        },
+        {
+          name: "本月收藏订单",
+          value: 210,
+          icon: "star-on",
+          color: "#ffb980"
+        },
+        {
+          name: "本月未支付订单",
+          value: 1234,
+          icon: "s-goods",
+          color: "#5ab1ef"
+        },
+      ]
     };
   },
 };
